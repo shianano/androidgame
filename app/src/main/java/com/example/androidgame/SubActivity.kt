cbac8c795c69db74183d853541598b018e87b60d
takuan0426 <takuan0426@icloud.com>
2022-06-27 13:16:15 +0900

    test

--- app/src/main/java/com/example/androidgame/SubActivity.kt
+++ app/src/main/java/com/example/androidgame/SubActivity.kt
@@ -2,8 +2,11 @@ package com.example.androidgame
 
 import android.os.Bundle
 import android.text.TextUtils.replace
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.androidgame.databinding.ActivitySubBinding
 
 class SubActivity : AppCompatActivity() {
@@ -12,14 +15,19 @@ class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySubBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_sub)
 
-        binding.settingButton.setOnClickListener{
+        binding.settingButton.setOnClickListener {
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.container, SettingFragment())
                 addToBackStack(null)
+                setContentView(R.layout.fragment_setting)
                 commit()
             }
         }
+    }
+
+    private fun setContentView(root: ConstraintLayout) {
+
     }
 }
