baf81de39a295510ad5970b64e52f5dbb9ca15cc
lwo_ootu <lwo-rovaf-lov@outlook.jp>
2022-09-01 13:51:51 +0900

    技追加

--- app/src/main/java/com/example/androidgame/MainActivity.kt
+++ app/src/main/java/com/example/androidgame/MainActivity.kt
@@ -93,10 +93,10 @@ class MainActivity : AppCompatActivity() {
     //人間と戦闘時ステータスを入れて戦闘
 
     //技(ult) ultの番号・[0]ヒール系,[1]攻撃系　判定変数・判定変数による値(例：ヒール系の値(100)なら100回復)
-    val ult_type: Array<Int> = arrayOf(0,1,1,0)
-    val ult_result_num: Array<Int> = arrayOf(20,15,25,100)
+    val ult_type: Array<Int> = arrayOf(0,1,1,0,1)
+    val ult_result_num: Array<Int> = arrayOf(20,15,25,100,18)
     val ult_use_mp: Array<Int> = arrayOf(5,5,10,15)
-    val ult_name: Array<String> = arrayOf("ヒール", "暗黒斬り","斬鉄","フルヒール")
+    val ult_name: Array<String> = arrayOf("ヒール", "暗黒斬り","斬鉄","フルヒール","ティマイオス")
     var ult_select_name = ""
     //
     var masu_num = 0
@@ -366,7 +366,10 @@ class MainActivity : AppCompatActivity() {
     }
     //マス確認
     fun masu_checker(num: Int){
-        if(masu_event[num]==1){
+        if(num==0){
+            //説明欄
+        }
+        else if(masu_event[num]==1){
             //戦闘
             binding.result.text = "戦闘！！"
             binding.no.text = masu_result_num[num].toString()
